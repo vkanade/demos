e4cdd0bf3ca9668d77905bd8b4e81569dd0c2fbf
vkanade <varunk@gmail.com>
2016-02-15 19:59:29 +0000

    Fix dataset-mnist.lua

--- train-a-digit-classifier/dataset-mnist.lua
+++ train-a-digit-classifier/dataset-mnist.lua
@@ -43,7 +43,7 @@ function mnist.loadDataset(fileName, maxLoad)
    local dataset = {}
    dataset.data = data
    dataset.labels = labels
-	dataset.binary_output = false
+	dataset.binary = false
 
    function dataset:normalize(mean_, std_)
       local mean = mean_ or data:view(data:size(1), -1):mean(1)
